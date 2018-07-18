bce9f6cbd1a152388e14ca53a84ba8f64e5940c0
Alex Enrique <alex@alex-enrique.alex-enrique>
2018-07-17 21:18:57 -0300

    clear

--- question2/js_view/ex/sketch.js
+++ question2/js_view/ex/sketch.js
@@ -2,25 +2,10 @@ function setup() {
   createCanvas(720,400);
   background(100);
 
-  var d = 70;
-  var p1 = d;
-  var p2 = p1+d;
-  var p3 = p2+d;
-  var p4 = p3+d;
-
-  line(p3, p3, p2, p3);
 }
 
 function draw() {
-  // rect(100,200, 75, 150);
 
-
-  line(100, 100, 100, 100);
-  stroke(250);
-  p2 = p2 + d/10;
-  line(p2, p3, p2, p2);
-  line(p2, p2, p3, p2);
-  line(p3, p2, p3, p3);
 
 
 
